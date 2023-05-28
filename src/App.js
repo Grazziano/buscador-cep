98bd74c9d5f73c40556b13fd72ac2de5aa5907f3
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-05-28 17:08:26 -0300

    Searching cep

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
 import './styles.css';
 
+import api from './services/api';
+
 function App() {
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === '') {
+      alert('É preciso informar um cep válido!');
+    }
+
+    try {
+      const response = await api.get(`${input}/json/`);
+      setCep(response.data);
+      setInput('');
+    } catch (error) {
+      alert('Ops');
+      setInput('');
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de Cep</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu cep..." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 79003222</h2>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua teste algum</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Vila rosa</span>
-        <span>Campo Grande - MS</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
