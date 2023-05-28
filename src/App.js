499e79aab6f32de13c627a1edab422c4afa70a1c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-05-28 16:47:45 -0300

    Styling page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador de Cep</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..." />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 79003222</h2>
+
+        <span>Rua teste algum</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Vila rosa</span>
+        <span>Campo Grande - MS</span>
+      </main>
     </div>
   );
 }
